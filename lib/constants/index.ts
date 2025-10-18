590dc89332df7e6f4337f37b2198aa56622250a4
karmartir <karmartir@gmail.com>
2025-10-17 20:11:10 -0400

    10. Root Layout & Constants

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Pro Store";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Modern e-commerce platform built with Next.js 13";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
