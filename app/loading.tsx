0574b95b5eea9998e97ade745e799cd3d6c005da
karmartir <karmartir@gmail.com>
2025-10-17 21:43:04 -0400

    13. Loading & Not Found Pages

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import loader from '@/assets/loader.gif'
+const LoadingPage = () => {
+    return ( 
+        <div className="flex justify-center items-center h-screen width-full">
+            <Image src={loader} height={150} width={150} alt="Loading..." />
+        </div>
+     );
+}
+ 
+export default LoadingPage;
---
+++ app/not-found.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image
+        src="/images/logo.svg"
+        width={48}
+        height={48}
+        alt={`${APP_NAME} logo`}
+        priority
+      />
+      <div className="p-6 rounded-lg w-1/3 shadow-md text-center">
+        <h6 className="text-3xl font-bold mb-4">Not Found</h6>
+        <p className="text-destructive">Could not find requested page</p>
+        <Button asChild variant="outline" className="mt-4">
+          <Link href="/">Back to Home</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
