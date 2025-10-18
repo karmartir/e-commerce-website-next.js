f9aae1e1c232df8205022747f7d22b3ca4e09613
karmartir <karmartir@gmail.com>
2025-10-18 12:42:16 -0400

    15. Sample Products & Product List (Add ProductList component and sample data for homepage display)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,12 @@
-
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 const Homepage = () => {
-  return (<>Pro-store</>  );
+
+
+  return (
+  <>
+  <ProductList data={sampleData.products} title="New Arrivals" limit={4} />
+  </>  );
 }
  
 export default Homepage;
