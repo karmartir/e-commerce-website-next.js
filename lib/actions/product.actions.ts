95b9131ac0ba28bb3e3d880a24dc39c8a4bd7f9f
karmartir <karmartir@gmail.com>
2025-10-21 21:25:57 -0400

    25. Product Details Page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,24 +1,20 @@
 "use server";
-//import { PrismaClient } from "@prisma/client";
 import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 export async function getLatestProducts() {
-  // Fetch latest products
-  //const prisma = new PrismaClient();
-  // Get latest products
+  // get latest products
   const data = await prisma.product.findMany({
     orderBy: { createdAt: "desc" },
     take: LATEST_PRODUCTS_LIMIT,
   });
 
-  // const formattedData = data.map((product) => ({
-  //   ...product,
-  //   price: Number(product.price),
-  //   rating: Number(product.rating),
-  // }));
-
   // Convert to plain object from prisma via utils
+  return convertToPlainObject(data);
+}
+// Get single product by it's slug
+export async function getProductBySlug(slug: string) {
+  const data = await prisma.product.findFirst({ where: { slug: slug } });
   return convertToPlainObject(data);
 }
