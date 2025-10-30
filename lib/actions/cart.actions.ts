cbd221e7e46a73ef70156700b7833c1fa134dd71
karmartir <karmartir@gmail.com>
2025-10-29 20:39:41 -0400

    48. Get Item From Cart

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,15 +1,43 @@
 "use server";
 import { CartItem } from "@/types";
 import { cookies } from "next/headers";
-import { formatError } from "../utils";
+import { convertToPlainObject, formatError } from "../utils";
+import { auth } from "@/auth";
+import { prisma } from "@/db/prisma";
+import { get } from "http";
+import { cartItemSchema } from "../validators";
 
 export async function addItemToCart(data: CartItem) {
   try {
     // Check for the cart cookie
     const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+    // If no cart cookie, throw an error
+    if (!sessionCartId) {
+      throw new Error("No session cart ID found");
+    }
+    // Get session and user Id
+    const session = await auth();
+    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+    // Get cart
+    const cart = await getMyCart();
+    // Parse and validate cart items
+    const item = cartItemSchema.parse(data);
+
+    // Find product in database
+    const product = await prisma.product.findFirst({
+      where: { id: item.productId },
+    });
+    if (!product) throw new Error("Product not found");
 
     // TESTING
-    console.log("Session Cart ID:", sessionCartId);
+    console.log({
+      "Session Cart ID": sessionCartId,
+      "User ID": userId,
+      "Item Requested": item,
+      "Product Found": product,
+    });
+
     return {
       success: true,
       message: "Item added to cart",
@@ -21,3 +49,30 @@ export async function addItemToCart(data: CartItem) {
     };
   }
 }
+
+export const getMyCart = async () => {
+  const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+  if (!sessionCartId) throw new Error("No session cart ID found");
+
+  // Get session and user Id
+  const session = await auth();
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+  // Get user cart from database
+  const cart = await prisma.cart.findFirst({
+    where: {
+      userId: userId ? userId : sessionCartId,
+    },
+  });
+  if (!cart) return undefined;
+
+  // Convert decimals and return cart
+  return convertToPlainObject({
+    ...cart,
+    items: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+  });
+};
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -7,8 +7,8 @@ export const SERVER_URL =
 export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.NEXT_PUBLIC_LATEST_PRODUCTS_LIMIT) || 6;
 export const signInDefaultValues = {
-  email: "",
-  password: "",
+  email: process.env.NEXT_PUBLIC_ADMIN_EMAIL || "",
+  password: process.env.NEXT_PUBLIC_ADMIN_PASSWORD || "",
 };
 export const signUpDefaultValues = {
   name: "",
