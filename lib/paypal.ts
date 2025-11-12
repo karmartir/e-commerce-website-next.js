aacdbb96332732907ae26b8eb16ec862d92a755a
karmartir <karmartir@gmail.com>
2025-11-12 17:00:34 -0500

    80. Create Order & Capture Payment Request

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,27 +1,73 @@
+import { ca } from "zod/v4/locales";
+
 const base = process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com";
 const clientId = process.env.PAYPAL_CLIENT_ID || "";
 const clientSecret = process.env.PAYPAL_CLIENT_SECRET || "";
-export const paypal = {}
+export const paypal = {
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "USD",
+              value: price,
+            },
+          },
+        ],
+      }),
+    });
+   return handleResponse(response);
+  },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return handleResponse(response);
+  }
+};
 
 //Generate paypal access token
-async function generateAccessToken(){
-    const {PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET} = process.env;
-    const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString("base64");
-    const response = await fetch(`${base}/v1/oauth2/token`, {
-        method: "POST",
-        headers: {
-            "Authorization": `Basic ${auth}`,
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({ grant_type: "client_credentials" }),
-    });
-    if(response.ok){
-        const jsonData = await response.json();
-        return jsonData.access_token
-    } else {
-        const errorMessage = await response.text();
-        throw new Error(errorMessage);
-    }
-}       
-    
+async function generateAccessToken() {
+  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
+  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
+    "base64"
+  );
+  const response = await fetch(`${base}/v1/oauth2/token`, {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${auth}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({ grant_type: "client_credentials" }),
+  });
+  
+    const jsonData = await handleResponse(response);
+    return jsonData.access_token;
+}
+
+async function handleResponse(response: Response) {
+  if (response.ok) {
+    const jsonData = await response.json();
+    return jsonData;
+  } else {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+}
+
 export { generateAccessToken };
