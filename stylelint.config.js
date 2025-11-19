0941e6479d2619d7e442101b1956f81b0156c3eb
karmartir <karmartir@gmail.com>
2025-11-18 21:53:40 -0500

    Section 10: Admin Overview & Orders 94. Admin Layout & Main Nav

---
+++ stylelint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // Only process CSS files
+  customSyntax: 'postcss',
+  // Ignore non-CSS files
+  ignoreFiles: [
+    '**/*.tsx',
+    '**/*.ts',
+    '**/*.js',
+    '**/*.jsx',
+    '**/*.json',
+    '**/node_modules/**',
+    '**/.next/**',
+    '**/out/**',
+    '**/build/**',
+    '**/dist/**',
+  ],
+  rules: {
+    // Add any stylelint rules you want here
+  },
+};
+
+
