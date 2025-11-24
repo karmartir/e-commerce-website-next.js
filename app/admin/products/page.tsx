272d599fe7b6015dc881904e34ef9ff5b3d3ffdd
karmartir <karmartir@gmail.com>
2025-11-23 22:09:31 -0500

    105. Display Products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from "react";
 import { requireAdmin } from "@/lib/auth-guard";
-import Link from 'next/link';
-import { getAllProducts } from '@/lib/actions/product.actions';
+import Link from "next/link";
+import { getAllProducts } from "@/lib/actions/product.actions";
 import { formatCurrency, formatId } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Pagination from "@/components/shared/pagination";
 
 const AdminProductsPage = async (props: {
   searchParams: Promise<{
     page: string;
     query: string;
     category: string;
-  }>
+  }>;
 }) => {
   await requireAdmin();
   const searchParams = await props.searchParams;
@@ -19,18 +29,56 @@ const AdminProductsPage = async (props: {
   const products = await getAllProducts({
     query: searchText,
     page,
-    category
-  })
+    category,
+  });
 
-  console.log(products);
   return (
-    <div className='space-y-2'>
-      <div className='flex-between'>
-        <h2 className='h2-bold'>Admin Products Dashboard</h2>
-
+    <div className="space-y-2">
+      <div className="flex-between">
+        <h1 className="h2-bold">Admin Products Dashboard</h1>
+        <Button asChild variant="default">
+          <Link href="/admin/products/create">Create Product</Link>
+        </Button>
       </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>NAME</TableHead>
+            <TableHead className="text-right">PRICE</TableHead>
+            <TableHead>CATEGORY</TableHead>
+            <TableHead>STOCK</TableHead>
+            <TableHead>RATING</TableHead>
+            <TableHead className="w-[100px]">ACTIONS</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {products.data.map((product) =>(
+            <TableRow key={product.id}>
+              <TableCell>{formatId(product.id)}</TableCell>
+              <TableCell>{product.name}</TableCell>
+              <TableCell className="text-right">{formatCurrency(product.price)}</TableCell>
+              <TableCell>{product.category}</TableCell>
+              <TableCell>{product.stock}</TableCell>
+              <TableCell>{product.rating}</TableCell>
+              <TableCell className="flex gap-1">
+                <Button asChild variant='outline' size='sm'>
+                  <Link href={`/admin/products/${product.id}`}>
+                  Edit
+                  </Link>
+                  {/* TODO delete button */}
+                </Button>
+              </TableCell>
+
+            </TableRow>
+          ) )}
+        </TableBody>
+      </Table>
+      {products?.totalPages && products.totalPages > 1 && (
+        <Pagination page={page} totalPages={products.totalPages}/>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default AdminProductsPage;
