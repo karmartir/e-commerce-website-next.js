a527b6655194df27155165b2977a889d27fd32bb
karmartir <karmartir@gmail.com>
2025-11-29 13:26:06 -0500

    128. Search Component

---
+++ components/shared/header/search.tsx
@@ -0,0 +1,32 @@
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { getAllCategories } from "@/lib/actions/product.actions";
+import { Button } from "@/components/ui/button";
+import { SearchIcon } from "lucide-react";
+
+const Search = async () => {
+    const categories = await getAllCategories();
+
+    return <form action='/search' method="GET">
+        <div className="flex w-full max-w-sm items-center space-x-2">
+            <Select name="category">
+                <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="All Categories" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem key="all" value="all">All Categories</SelectItem>
+
+                    {categories.map((x) => (
+                        <SelectItem key={x.category} value={x.category}>
+                            {x.category}
+                        </SelectItem>
+                    ))}
+
+                </SelectContent>
+            </Select>
+            <Input name="q" type="text" placeholder="Search..." className="md:w-[100px] lg:w-[300px]" />
+            <Button> <SearchIcon/></Button>
+        </div>
+    </form>;
+}
+export default Search;
