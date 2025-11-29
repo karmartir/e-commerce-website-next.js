e47d8908ce5245fa102350b57cd2a1a16bcc7647
karmartir <karmartir@gmail.com>
2025-11-29 11:45:26 -0500

    127. Featured Products Carousel

--- components/shared/product/product-carousel.tsx
+++ components/shared/product/product-carousel.tsx
@@ -1,8 +1,40 @@
 'use client';
-const ProductCarousel = () => {
-    return ( 
-        <div>Product Carousel</div>
-     );
+
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import { Product } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+
+const ProductCarousel = ({ data }: {
+    data: Product[]
+}) => {
+    return (
+        <Carousel className="w-full mb-12" opts={{
+            loop: true
+        }} plugins={[Autoplay(
+            { delay: 8000, stopOnInteraction: true, stopOnMouseEnter: true }
+        )]}>
+            <CarouselContent>
+                {data.map((product: Product) => (
+                    <CarouselItem key={product.id} className="p-4">
+                        <Link href={`/product/${product.slug}`} className="block border rounded-lg p-4 hover:shadow-lg transition-shadow">
+                            <div className="relative mx-auto">
+                                <Image src={product.banner!} alt={product.name} width="0" height="0" sizes="100vw" className="w-full h-auto" />
+                                <div className="absolute inset-0 flex items-end justify-center">
+                                    <h2 className="bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white">{product.name}</h2>
+                                </div>
+                            </div>
+                        </Link>
+
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+
+        </Carousel>
+    );
 }
- 
+
 export default ProductCarousel;
