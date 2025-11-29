b2991fc849e96d955cb62773cf0302c258f29e8d
karmartir <karmartir@gmail.com>
2025-11-29 14:56:08 -0500

    129. Search Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts, getFeaturedProducts } from "@/lib/actions/product.actions";
 import ProductCarousel from "@/components/shared/product/product-carousel";
+import ViewAllProductsButton from "@/components/view-all-products-button";
 const Homepage = async () => {
   // Fetch latest products from the database via product.actions
   const latestProducts = await getLatestProducts();
@@ -10,6 +11,7 @@ const Homepage = async () => {
     <>
     {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
       <ProductList data={latestProducts} title="New Arrivals" limit={4} />
+      <ViewAllProductsButton />
     </>
   );
 };
