c0cfa8c098ef5a5be226270ecc67b0ec63ac62e5
karmartir <karmartir@gmail.com>
2025-11-29 20:09:14 -0500

    131. Get Filter URL Function

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -12,6 +12,69 @@ const SearchPage = async (props: {
     }>
 }) => {
     const { q = 'all', category = 'all', price = 'all', rating = 'all', sort = 'newest', page = '1' } = await props.searchParams;
+    
+    // Construct the filter url
+
+    /* const getFilterUrl = ({
+        c,
+        p,
+        s,
+        r,
+        pg
+    }: {
+        c?: string;
+        p?: string;
+        s?: string;
+        r?: string;
+        pg?: string;
+    }) => {
+        const params = {q, category, price, rating, sort, page};
+        
+        if (c) params.category = c;
+        if (p) params.price = p;
+        if (s) params.sort = s;
+        if (r) params.rating = r;
+        if (pg) params.page = pg;
+
+        return `/search?` + new URLSearchParams(params).toString();
+
+    } */
+const getFilterUrl = ({
+    c,
+    p,
+    s,
+    r,
+    pg
+}: {
+    c?: string;
+    p?: string;
+    s?: string;
+    r?: string;
+    pg?: string;
+}) => {
+    const params = {
+        q,
+        category,
+        price,
+        rating,
+        sort,
+        page
+    };
+
+    if (c) params.category = c;
+    if (p) params.price = p;
+    if (s) params.sort = s;
+    if (r) params.rating = r;
+    if (pg) params.page = pg;
+
+    // manually encode each param so spaces become %20
+    const queryString = Object.entries(params)
+        .map(([key, value]) => `${key}=${encodeURIComponent(value as string)}`)
+        .join('&');
+
+    return `/search?` + queryString;
+};
+
     const products = await getAllProducts({
         query: q,
         category,
@@ -24,6 +87,7 @@ const SearchPage = async (props: {
         <div className="grid md:grid-cols-5 md:gap-5">
             <div className="filter-links">
                 {/* {Filters} */}
+                URL: {getFilterUrl({c: 'Mens Sweat Shirts'})}
             </div>
 
             <div className="md:col-span-4 space-y-4">
