ad6212882491a0264bed7039f69879090dc19230
karmartir <karmartir@gmail.com>
2025-12-02 00:31:52 -0500

    email sender

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -7,12 +7,13 @@ import { getMyCart } from "./cart.actions";
 import { getUserById } from "./user.actions";
 import { insertOrderSchema } from "../validators";
 import { prisma } from "@/db/prisma";
-import { CartItem, PaymentResult } from "@/types";
+import { CartItem, PaymentResult, ShippingAddress } from "@/types";
 import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
 import { PAGE_SIZE } from "../constants";
 import { Prisma } from "@prisma/client";
 import { requireAdmin } from "../auth-guard";
+import { sendPurchaseReceipt } from "@/email";
 
 //Create Order and create the order items
 
@@ -240,7 +241,15 @@ export async function updateOrderToPaid({
     if (!updatedOrder) {
       throw new Error("Order not found");
     }
+    sendPurchaseReceipt({
+      order: {
+        ...updatedOrder,
+        shippingAddress: updatedOrder.shippingAddress as ShippingAddress,
+        paymentResult: updatedOrder.paymentResult as PaymentResult,
+      }
+    })
   });
+
 }
 
 // Get user's orders
