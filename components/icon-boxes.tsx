58ac4364732ef1e747113d0869808412f9837db6
karmartir <karmartir@gmail.com>
2025-12-02 13:04:56 -0500

    aligned by center icons

--- components/icon-boxes.tsx
+++ components/icon-boxes.tsx
@@ -5,30 +5,30 @@ const IconBoxes = () => {
     return ( 
         <div>
             <Card className="bg-background/50 shadow-md border border-border/50">
-                <CardContent className="grid md:grid-cols-4 gap-4 p-4">
+                <CardContent className="grid md:grid-cols-4 gap-4 p-4 text-center md:text-left">
                     <div className="space-y-2 border-r pr-4 md:border-r md:pr-4">
-                        <ShoppingBag/>
+                        <ShoppingBag className="mx-auto md:mx-0"/>
                         <div className='text-sm font-bold'>Free Shipping</div>
                         <div className="text-sm text-muted-foreground">
                             Free shipping on orders above $100
                         </div>
                     </div>
-                     <div className="space-y-2 border-r pr-4 md:border-r md:pr-4">
-                        <DollarSign/>
+                    <div className="space-y-2 border-r pr-4 md:border-r md:pr-4">
+                        <DollarSign className="mx-auto md:mx-0"/>
                         <div className='text-sm font-bold'>Money Back Guarantee</div>
                         <div className="text-sm text-muted-foreground">
                            Within 30 days of purchase
                         </div>
                     </div>
-                      <div className="space-y-2 border-r pr-4 md:border-r md:pr-4">
-                        <WalletCards/>
+                    <div className="space-y-2 border-r pr-4 md:border-r md:pr-4">
+                        <WalletCards className="mx-auto md:mx-0"/>
                         <div className='text-sm font-bold'>Flexible Payment</div>
                         <div className="text-sm text-muted-foreground">
                             Pay with credit card, PayPal or COD
                         </div>       
                     </div>
                     <div className="space-y-2">
-                        <Headset/>
+                        <Headset className="mx-auto md:mx-0"/>
                         <div className='text-sm font-bold'>24/7 Support</div>
                         <div className="text-sm text-muted-foreground">
                             Get support at any time
