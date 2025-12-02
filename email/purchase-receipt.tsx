f501f8438aa04540401b61dc84d5f911c56f6b2a
karmartir <karmartir@gmail.com>
2025-12-01 22:53:20 -0500

    153. Resend Main Function

---
+++ email/purchase-receipt.tsx
@@ -0,0 +1,12 @@
+import { Order } from '@/types'
+import React from 'react'
+
+const PurchaseReceiptEmail = ({order}: {order: Order}) => {
+  return (
+    <>
+      Email
+    </>
+  )
+}
+
+export default PurchaseReceiptEmail
